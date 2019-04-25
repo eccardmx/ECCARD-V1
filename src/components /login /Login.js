cef1a9a49bcd836caea87b6face312e1488d88b8
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-04-24 22:25:03 -0500

    update login and regiter compoent created

---
+++ src/components /login /Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        <div className="login-wrapper">Login component</div>
+      </div>
+    );
+  }
+}
+
+export default Login;
---
+++ src/components /register/Register.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Register extends Component {
+  render() {
+    return (
+      <div>
+        <div className="register-wrapper">Register Component</div>
+      </div>
+    );
+  }
+}
+
+export default Register;
